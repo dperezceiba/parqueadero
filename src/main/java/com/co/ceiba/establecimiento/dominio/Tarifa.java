8f52b721fb90a9923d4edad42151149f606a69d8
dario.perez <dario.perez@ceiba.com.co>
2019-02-14 10:06:58 -0500

    Fix unit test tarifas y utilidades

--- src/main/java/com/co/ceiba/establecimiento/dominio/Tarifa.java
+++ src/main/java/com/co/ceiba/establecimiento/dominio/Tarifa.java
@@ -4,10 +4,6 @@ public class Tarifa {
 	private Double valor;
 	private String tipoVehiculo;
 	private String modalidad;
-
-	public Tarifa() {
-		super();
-	}
 	
 	public Tarifa(Double valor){
 		this.valor = valor;
@@ -23,24 +19,12 @@ public class Tarifa {
 		return valor;
 	}
 
-	public void setValor(Double valor) {
-		this.valor = valor;
-	}
-
 	public String getTipoVehiculo() {
 		return tipoVehiculo;
 	}
 
-	public void setTipoVehiculo(String tipoVehiculo) {
-		this.tipoVehiculo = tipoVehiculo;
-	}
-
 	public String getModalidad() {
 		return modalidad;
-	}
-
-	public void setModalidad(String modalidad) {
-		this.modalidad = modalidad;
 	}
 
 }
--- src/test/java/com/co/ceiba/establecimiento/unitaria/UtilidadesTest.java
+++ src/test/java/com/co/ceiba/establecimiento/unitaria/UtilidadesTest.java
@@ -3,6 +3,7 @@ package com.co.ceiba.establecimiento.unitaria;
 import static org.junit.Assert.assertNull;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,9 +11,15 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.co.ceiba.establecimiento.builder.CarroBuilder;
+import com.co.ceiba.establecimiento.builder.EntradaBuilder;
 import com.co.ceiba.establecimiento.builder.MotoBuilder;
 import com.co.ceiba.establecimiento.builder.SalidaBuilder;
+import com.co.ceiba.establecimiento.dominio.Carro;
+import com.co.ceiba.establecimiento.dominio.Entrada;
+import com.co.ceiba.establecimiento.dominio.Moto;
+import com.co.ceiba.establecimiento.dominio.Salida;
 import com.co.ceiba.establecimiento.entidad.CarroEntity;
+import com.co.ceiba.establecimiento.entidad.EntradaEntity;
 import com.co.ceiba.establecimiento.entidad.MotoEntity;
 import com.co.ceiba.establecimiento.entidad.SalidaEntity;
 import com.co.ceiba.establecimiento.util.FechaUtils;
@@ -27,12 +34,27 @@ public class UtilidadesTest {
 		CarroEntity carroEntity = null;
 		SalidaEntity salidaEntity = null;
 		Timestamp timestamp = null;
+		EntradaEntity entradaEntity = null;
+		Entrada entrada = null;
+		Salida salida = null;
+		Moto moto = null;
+		Carro carro = null;
 
 		assertNull(MotoBuilder.convertirADominio(motoEntity));
 		assertNull(CarroBuilder.convertirADominio(carroEntity));
 		assertNull(SalidaBuilder.convertirADominio(salidaEntity));
 		assertNull(FechaUtils.convertir(timestamp));
-
+		assertNull(EntradaBuilder.convertirADominio(entradaEntity));
+		assertNull(EntradaBuilder.convertirAEntity(entrada));
+		assertNull(SalidaBuilder.convertirAEntity(salida));
+		assertNull(MotoBuilder.convertirAEntity(moto));
+		assertNull(CarroBuilder.convertirAEntity(carro));
+	}
+	
+	@Test
+	public void probarFechas() {
+		LocalDateTime fecha = null;
+		assertNull(FechaUtils.convertir(fecha));
 	}
 
 }
